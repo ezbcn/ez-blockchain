9579ca59ef0fb93e27ead1f100132e14f13d2629
Guillermo Tala <guille86598@gmail.com>
2018-09-10 03:16:30 +0000

    server creado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import { createServer } from './server.js'
-
-// createServer((request, response) => {
-//     response.writeHead(200, { 'Content-Type': 'text/html' });
-//     response.write('Server TEST');
-//     response.end();
-// })
-
-
 createServer()
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import index from '../routes/index.js'
 
 export const createServer = async() => {
     const app = express();
+    app.use('/', index);
     app.listen(process.env.PORT, process.env.IP);
     return app
 }
