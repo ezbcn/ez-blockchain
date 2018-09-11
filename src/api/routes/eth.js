2354ea7192829786bf4d50d63b6ad787ba765457
Guillermo Tala <guille86598@gmail.com>
2018-09-11 17:11:52 +0000

    test

--- src/api/routes/eth.js
+++ src/api/routes/eth.js
@@ -6,11 +6,14 @@ eth.get('/:acc', (req, res) => {
     try {
 
         const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/"));
+        
         var account = req.url.split('/')[1];
         var balance = web3.eth.getBalance(account);
         balance = web3.toDecimal(balance);
+        
         console.log(balance);
         balance = balance.toString();
+        res.setHeader('Content-Type', 'text/plain');
         res.status(200).send(balance);
 
     }
--- src/server.js
+++ src/server.js
@@ -4,9 +4,12 @@ import index from './api/routes/index.js'
 import eth from './api/routes/eth.js'
 
 export const createServer = async() => {
+    
     const app = express();
     app.use('/', index);
     app.use('/eth/', eth);
-    app.listen(process.env.PORT, process.env.IP);
+    const PORT = process.env.PORT || 5000;
+    
+    app.listen(PORT, () => { console.log(`Server running on ${PORT}/`); });
     return app
 }
