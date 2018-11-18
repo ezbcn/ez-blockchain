16c57054fd824d6b6cb22c917c0bba0871d820d7
Guillermo Tala <guille86598@gmail.com>
2018-11-18 15:55:40 +0000

    .

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ import ethMainnet from './api/routes/ethereum/mainnet/ethMainnet.js'
 import btcTestnet from './api/routes/bitcoin/testnet/btcTestnet.js'
 import btcMainnet from './api/routes/bitcoin/mainnet/btcMainnet.js'
 
+import rskTestnet from './api/routes/rsk/testnet/rskTestnet.js'
+import rskMainnet from './api/routes/rsk/mainnet/rskMainnet.js'
+
 //TODO: implementar seguridad para acceso Nde cliente a esta API | 
 
 export const createServer = async() => {
@@ -16,6 +19,9 @@ export const createServer = async() => {
     
     app.use('/bitcoin/testnet/', btcTestnet);
     app.use('/bitcoin/mainnet/', btcMainnet);
+    
+    app.use('/rsk/testnet/', rskTestnet);
+    app.use('/rsk/mainnet/', rskMainnet);
     
     //Valida que la dirección sea existente
     app.use((req, res) => { res.status(404).send('Error 404 - Dirección no encontrada'); });
